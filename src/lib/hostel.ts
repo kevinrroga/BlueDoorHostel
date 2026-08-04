5f695b41fd55887defb1101f98533100a5c7ccaf
Kevin <kevinrroga3@gmail.com>
2026-08-04 14:22:35 +0200

    Add global styles and utility helpers

---
+++ src/lib/hostel.ts
@@ -0,0 +1,10 @@
+export const BOOKING_URL =
+  "https://www.hostelworld.com/hostels/p/312998/blue-door-hostel/";
+
+export const ADDRESS = "Rruga Shtjefen Gjecovi 18, Tirana, Albania";
+
+export const COORDS = { lat: 41.3336245, lng: 19.8288341 };
+
+export const MAP_EMBED_URL = `https://www.google.com/maps?q=${COORDS.lat},${COORDS.lng}&z=16&output=embed`;
+
+export const DIRECTIONS_URL = `https://www.google.com/maps/dir/?api=1&destination=${COORDS.lat},${COORDS.lng}`;
